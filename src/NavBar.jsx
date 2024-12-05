e5e2d7274e7705846a012ae8de1e0b6d5a85cfe5
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-12-05 11:20:44 -0800

    added Materials UI and imports

---
+++ src/NavBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
